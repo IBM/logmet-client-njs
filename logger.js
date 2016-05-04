1b99174fc093a298f7a312baf0fea23864756a47
hermanba <hermanba@ca.ibm.com>
2016-05-04 11:27:53 -0400

    Allow clients to customize logging level for module

--- logger.js
+++ logger.js
@@ -4,6 +4,6 @@
 
 var bunyan = require('bunyan');
 
-var logger = bunyan.createLogger({name:'logmet_client', src:false, level:'debug'});
+var logger = bunyan.createLogger({name:'logmet_client', src:false, level: process.env.logmet_client_njs_level || 'warn'});
 
 module.exports = logger;
