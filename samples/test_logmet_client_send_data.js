564922530e151c6da63721b8447accaf3439c661
FABIO A. OLIVEIRA <fabolive@us.ibm.com>
2016-06-28 09:12:44 -0400

    Bumped up module version.

--- samples/test_logmet_client_send_data.js
+++ samples/test_logmet_client_send_data.js
@@ -10,7 +10,7 @@ var tenantId = process.env.TENANT_ID;
 var logmetToken = process.env.LOGMET_TOKEN;
 
 // The Elasticsearch type of the documents to be written to Logmet
-var type = 'fabio-test-5';
+var type = 'fabio-test-6';
 
 // How many data points will be produced when running this sample
 var NUMBER_OF_DATA_POINTS = 200;
